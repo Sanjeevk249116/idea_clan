ca183dac3238293f2c7c143d26cd45bc5c614cbd
Sanjeev Kushwaha <sanjeevk249116@gmail.com>
2024-03-11 23:16:34 +0530

    color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./navbar/NavBar";
 import MainRouter from "./routers/MainRouter";
 function App() {
   return (
-    <div className=" h-full  maincss">
+    <div className="   maincss">
     
       <MainRouter />
     </div>
